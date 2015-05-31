37c8fb66d9329314461d6b8ceefea5ac4c9fa19b
Abhay Kumar Singh <abhayawesome@gmail.com>
2015-05-31 18:23:03 +0530

    Automates processing of log and writes in database
    Note here database is simple exel file. Function names have not been given
    properly. Some temporary noise (changes) is also present in this.

--- execAllPairs.py
+++ execAllPairs.py
@@ -1,18 +1,52 @@
 #!C:/Python34/python.exe
 import eventFileCreator
 import os
+import logFileParser as lf
+import csv
+import re
+
+def fun( path ):
+	filename = os.path.basename( path )
+	filename = os.path.splitext( filename )[0]
+	matchObj = re.match('([fc])([a-z])t(\d+)n(\d+)s(\d+)p', filename )
+	if matchObj.group(1) == 'f' :
+		typ = 'FloodRun'
+	elif matchObj.group(1) == 'c' :
+		typ = 'ConsRun'
+	else :
+		print('Something wrong in filename type')
+	return [ matchObj.group(1), matchObj.group(2), 't', matchObj.group(3), 'n',
+		matchObj.group(4), 's', matchObj.group(5), 'p' ], typ
+
+def processCsv():
+	# read from recentlog
+	with open( 'E:/ResourcesAndOutpForThesis/UsefulScriptsThesis/recentlog.txt', 'r' ) as f :
+		toBeProcessed = f.readline()
+	# find out what kind of log it is
+	astring, typ = fun( toBeProcessed )
+	# process it
+	ana = lf.Analysis(lf.SheetReader( toBeProcessed ), typ )
+	astring.append(str(ana.avgOfAvgOfWaitTimes()))
+	astring.append(str(ana.avgOfOvAllUsefulComps()))
+	astring.append(str(ana.avgOfOvallUsefulCommun()))
+	ana.cleanup()
+	# write in ....
+	with open( 'E:/ResourcesAndOutpForThesis/UsefulScriptsThesis/filename.csv', 'a', newline='' ) as f:
+		wri = csv.writer(f)
+		wri.writerow(astring)
 
 def executePossiblities():
 	eventFileCreator.flooding = 'E:/Typhon Test-bed/events.pl'
 	eventFileCreator.conscientious = 'E:/MobileAgentPart/events.pl'
-	eventFileCreator.totalNumNodes = 9
+	eventFileCreator.totalNumNodes = 16
+	eventFileCreator.topology = 'g'
 	eventFileCreator.availableSol = (('type2','a',5000),)
 	eventFileCreator.pattern = (('i',),(8,'s'),(1,'p'))
 	eventFileCreator.timeGaps = (0,4000,3000)
 	eventFileCreator.originTime = 0
 	eventFileCreator.reps = 5
-	eventFileCreator.timeBwTwoFresh = 5000
-	for numOfSol, numOfProb in genPairSolProb( 1, 1, 9 ):
+	eventFileCreator.timeBwTwoFresh = 10000
+	for numOfSol, numOfProb in genPairSolProb( 8, 4, 16 ):
 		print( numOfSol, numOfProb)
 		eventFileCreator.pattern = (('i',),(numOfSol,'s'),(numOfProb,'p'))
 		# create the event file
@@ -21,9 +55,11 @@ def executePossiblities():
 		# execute flood
 		os.chdir('E:/Typhon Test-bed')
 		os.system('autom.bat')
+		processCsv()
 		# execute cons
 		os.chdir('E:/MobileAgentPart')
 		os.system('autom.bat')
+		processCsv()
 		os.chdir( originalPath )
 
 # genPairSolProb is a generator which generate the different pairs from
